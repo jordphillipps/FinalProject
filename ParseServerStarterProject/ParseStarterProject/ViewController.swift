93ce140a3eeddc258aa2bc5a8e2d6be734c18c2b
Jordan Phillipps <Jordan1@MacBook-Pro.home>
2016-03-30 21:55:59 +0100

    Added the code behind the login screen, data is parsing to database, error messages all work

--- ParseServerStarterProject/ParseStarterProject/ViewController.swift
+++ ParseServerStarterProject/ParseStarterProject/ViewController.swift
@@ -13,11 +13,162 @@ import CoreData
 
 
 class ViewController: UIViewController {
+    
+    
+    var signupActive = true
+    
+    @IBOutlet var username: UITextField!
+    
+    @IBOutlet var password: UITextField!
+    
+    @IBOutlet var signupButton: UIButton!
+    
+    @IBOutlet var registeredText: UILabel!
+    
+    @IBOutlet var loginButton: UIButton!
+    
+    
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    
+    func displayAlert(title: String, message: String) {
+        
+        var alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alert.addAction((UIAlertAction(title: "Ok", style: .Default, handler: { (action) -> Void in
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+            
+        })))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+        
+        
+    }
+    
+    @IBAction func signUp(sender: AnyObject) {
+    
+        //Error message for when the user hasn't entered username and password credentials.
+        if username.text == "" || password.text == "" {
+            
+          
+            displayAlert("Error in form", message: "Please enter a username and password")
+            
+        }else {
+            
+            //Begin activity indicator to sign up the user
+            activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50))
+            activityIndicator.center = self.view.center
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+            view.addSubview(activityIndicator)
+            activityIndicator.startAnimating()
+            UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+            
+            
+            var errorMessage = "Please try again later"
+            
+            if signupActive == true {
+            
+            var user = PFUser()
+            user.username = username.text
+            user.password = password.text
+            
+            
+            
+            user.signUpInBackgroundWithBlock({ (success, error) -> Void in
+                
+                self.activityIndicator.stopAnimating()
+                UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                
+                if error == nil {
+                    //signup succesful 
+                    
+                    
+                    
+                    
+                } else {
+                    
+                    if let errorString = error!.userInfo["error"] as? String {
+                        
+                        errorMessage = errorString
+                        
+                    }
+                    
+                    self.displayAlert("Failed to sign up", message: errorMessage)
+                    
+                    
+                }
+                
+                
+            })
+            
+            
+            }else {
+                
+                PFUser.logInWithUsernameInBackground(username.text!, password: password.text!, block: { (user, error) -> Void in
+                    
+                    self.activityIndicator.stopAnimating()
+                    UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                    
+                    if user != nil {
+                        
+                        //logged in!
+                        
+                    }else {
+                        
+                        if let errorString = error!.userInfo["error"] as? String {
+                            
+                            errorMessage = errorString
+                        }
+                        
+                        self.displayAlert("Failed to login", message: errorMessage)
+                        
+                    }
+                    
+                })
+            }
+            
+        }
+       
+        
+    }
 
+    
+    
+    @IBAction func logIn(sender: AnyObject) {
+        
+        if signupActive == true {
+            
+            signupButton.setTitle("Login", forState: UIControlState.Normal)
+            
+            registeredText.text = "Not Registered?"
+            
+            loginButton.setTitle("Sign Up", forState: UIControlState.Normal)
+            
+            signupActive = false
+            
+        }else {
+            
+            signupButton.setTitle("Sign Up", forState: UIControlState.Normal)
+            
+            registeredText.text = "Already Registered?"
+            
+            loginButton.setTitle("Login", forState: UIControlState.Normal)
+            
+            signupActive = true
+            
+            
+            
+        }
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         
         }
 
