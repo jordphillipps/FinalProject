3c109f0b5ccda12869f37ca5cf02bafec925f412
Jordan Phillipps <Jordan1@MacBook-Pro.home>
2016-04-04 16:40:33 +0100

    clean project, with menu and core data added, restarted due to errors

---
+++ FinalProjectApp/FinalProjectAppTests/FinalProjectAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  FinalProjectAppTests.swift
+//  FinalProjectAppTests
+//
+//  Created by Jordan Phillipps on 04/04/2016.
+//  Copyright © 2016 JordanPhillipps. All rights reserved.
+//
+
+import XCTest
+@testable import FinalProjectApp
+
+class FinalProjectAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
