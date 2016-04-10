35254835f1ebbf866307c09cbca131d2fcffc517
Jordan Phillipps <Jordan1@MacBook-Pro.home>
2016-04-10 12:11:06 +0100

    Added the final menu buttons

---
+++ FinalProjectApp/FinalProjectApp/ViewController7.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController7.swift
+//  FinalProjectApp
+//
+//  Created by Jordan Phillipps on 10/04/2016.
+//  Copyright © 2016 JordanPhillipps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController7: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = "revealToggle:"
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
